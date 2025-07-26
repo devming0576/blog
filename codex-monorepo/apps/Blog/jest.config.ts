65eb1e4b42c1eab704034035954619ef93bcbc3c
Yiming0576 <g1097420948@gmail.com>
2025-07-25 22:52:29 -0400

    setup the frontend and backend

---
+++ codex-monorepo/apps/Blog/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@codex-monorepo/Blog',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/Blog',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
